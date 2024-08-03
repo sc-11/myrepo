c7115dd840f4b1a4d5738b15822137667329e39d
sc-11 <sebastian.cosentino@dcmail.ca>
2024-08-02 22:12:42 -0400

    beginning code provided has been pasted into doc

---
+++ assignment4/part2.js
@@ -0,0 +1,24 @@
+/*	Name: Sebastian Cosentino
+	File: part2.js
+	Date: August 2, 2024
+	Purpose: Part 2 js of assignment 4
+    */
+
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Declaring the array of image filenames */
+
+/* Declaring the alternative text for each image file */
+
+/* Looping through images */
+
+const newImage = document.createElement('img');
+newImage.setAttribute('src', xxx);
+newImage.setAttribute('alt', xxx);
+thumbBar.appendChild(newImage);
+
+/* Wiring up the Darken/Lighten button */
