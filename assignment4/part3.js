57d4ad42612f19f3d447df8141ca91f7cc777580
sc-11 <sebastian.cosentino@dcmail.ca>
2024-08-02 23:28:47 -0400

    added ball and movement function

--- assignment4/part3.js
+++ assignment4/part3.js
@@ -5,22 +5,54 @@
 	Purpose: Part 3 js of assignment 4
     */
 
-// setup canvas
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
 
-const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
-
-const width = (canvas.width = window.innerWidth);
-const height = (canvas.height = window.innerHeight);
-
-// function to generate random number
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 function random(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// function to generate random color
-
-function randomRGB() {
-  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+function randomColor() {
+    return 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')';
 }
+
+function Ball(x, y, velX, velY, color, size) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
+}
+
+Ball.prototype.draw = function() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+};
+
+Ball.prototype.update = function() {
+    if ((this.x + this.size) >= canvas.width) {
+        this.velX = -(this.velX);
+    }
+
+    if ((this.x - this.size) <= 0) {
+        this.velX = -(this.velX);
+    }
+
+    if ((this.y + this.size) >= canvas.height) {
+        this.velY = -(this.velY);
+    }
+
+    if ((this.y - this.size) <= 0) {
+        this.velY = -(this.velY);
+    }
+
+    this.x += this.velX;
+    this.y += this.velY;
+};
+
