ad777714b77a6b51c2fc4c613cf919304a75c8a9
sc-11 <sebastian.cosentino@dcmail.ca>
2024-08-02 23:02:06 -0400

    added pictures, made pics array

--- assignment4/part2.js
+++ assignment4/part2.js
@@ -4,21 +4,21 @@
 	Purpose: Part 2 js of assignment 4
     */
 
-const displayedImage = document.querySelector('.displayed-img');
-const thumbBar = document.querySelector('.thumb-bar');
 
-const btn = document.querySelector('button');
-const overlay = document.querySelector('.overlay');
+    const displayedImage = document.querySelector('.displayed-img');
+    const thumbBar = document.querySelector('.thumb-bar');
+    
+    const btn = document.querySelector('button');
+    const overlay = document.querySelector('.overlay');
+    
+    
+    const pics = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
+    const alts = {
+      'pic1.jpg' : 'Closeup of a human eye',
+      'pic2.jpg' : 'Rock that looks like a wave',
+      'pic3.jpg' : 'Purple and white pansies',
+      'pic4.jpg' : 'Section of wall from a pharoah\'s tomb',
+      'pic5.jpg' : 'Large moth on a leaf'
+    }
+    
 
-/* Declaring the array of image filenames */
-
-/* Declaring the alternative text for each image file */
-
-/* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
-
-/* Wiring up the Darken/Lighten button */
