d29cf8b1e80b1a4403c1c73172b8c861325bfe35
sc-11 <sebastian.cosentino@dcmail.ca>
2024-08-02 21:22:19 -0400

    created and started first part of js code based on the guidelines

---
+++ assignment4/part1.js
@@ -0,0 +1,17 @@
+/*	Name: Sebastian Cosentino
+	File: part1.js
+	Date: August 2, 2024
+	Purpose: Part 1 js of assignment 4
+    */
+
+const customName = document.getElementById('customname');
+const randomize = document.querySelector('.randomize');
+const story = document.querySelector('.story');
+
+function randomValueFromArray(array){
+  const random = Math.floor(Math.random() * array.length);
+  return array[random];
+}
+
+const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+
