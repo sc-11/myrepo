9a3468734c1464728ff99fcd8fcd8e14e1625bbf
sc-11 <sebastian.cosentino@dcmail.ca>
2024-08-02 23:03:16 -0400

    finished js loop, code done

--- assignment4/part2.js
+++ assignment4/part2.js
@@ -22,3 +22,27 @@
     }
     
 
+    for (const image of pics) {
+      const newImage = document.createElement('img');
+      newImage.setAttribute('src', `${image}`);
+      newImage.setAttribute('alt', alts[image]);
+      thumbBar.appendChild(newImage);
+      newImage.addEventListener('click', e => {
+        displayedImage.src = e.target.src;
+        displayedImage.alt = e.target.alt;
+      });
+    }
+    
+    
+    btn.addEventListener('click', () => {
+      const btnClass = btn.getAttribute('class');
+      if (btnClass === 'dark') {
+        btn.setAttribute('class','light');
+        btn.textContent = 'Lighten';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+      } else {
+        btn.setAttribute('class','dark');
+        btn.textContent = 'Darken';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+      }
+    });
